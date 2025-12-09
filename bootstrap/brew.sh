9ad9efcfb3a3760ea0546ff1941551a7365b8388
KhanhIceTea <khanhicetea@KhanhIceTeas-MacBook-Pro.local>
2025-12-09 16:47:30 +0700

    brew

--- bootstrap/brew.sh
+++ bootstrap/brew.sh
@@ -4,7 +4,7 @@ brew install hivemind
 brew install mkcert
 brew install caddy
 brew install redis@6.2
-brew install mysql@8.0
+brew install mysql@8.4
 brew install php@8.2
 brew install php@8.3
 brew install composer
